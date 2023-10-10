3106234d36ff9460588fadf8ec8c67cc90e11c99
imadeekosatria <imadeekosatriawiguna@gmail.com>
2023-10-10 11:44:39 +0800

    add reChaptcha

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore} from "firebase/firestore";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,6 +15,19 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
 };
 
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+if (process.env.VERCEL_ENV === 'development') {
+  self.FIREBASE_APPCHECK_DEBUG_TOKEN = process.env.APP_CHECK_DEBUG_TOKEN_FROM_CI;
+}
+initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6Lf7CI0oAAAAABJpzYkrUlujGkg3NBA_bRA0oUlo'),
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  isTokenAutoRefreshEnabled: true
+});
+
 export const db = getFirestore(app);
