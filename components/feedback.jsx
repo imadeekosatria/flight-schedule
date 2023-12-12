f6e64cc6e20b916eee14c02fcb4e45b06a4ef642
imadeekosatria <imadeekosatriawiguna@gmail.com>
2023-12-12 23:10:50 +0800

    adding feedback component (alpha)

--- components/feedback.jsx
+++ components/feedback.jsx
@@ -7,24 +7,64 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Textarea } from "@/components/ui/textarea"
 import { Button } from "./ui/button";
 import { useEffect, useState } from "react";
+import ReactStars from "react-rating-stars-component";
 
 export default function FeedBack() {
-  const [open, setOpen] = useState(false)
-  useEffect(()=>{
-    setTimeout(()=>{setOpen(true)},10000)
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setOpen(true);
+    }, 10000);
   }, [])
+
+  
+    const [rating, setRating] = useState(null)
+    const [feedback, setFeedback] = useState(null)
+    
+  
+  console.log(rating)
+  console.log(feedback)
   return (
     <>
-      <Dialog open={open} onOpenChange={()=>{setOpen(false)}}>
+      <Dialog
+        open={open}
+        onOpenChange={() => {
+          setOpen(false);
+        }}
+      >
         <DialogContent>
           <DialogHeader>
             <DialogTitle>
-              <span>How about your experience on this site?</span>
+              <div className="flex items-center gap-x-2">
+                Your Feedback
+                <box-icon type='solid' name='comment-detail'></box-icon>
+              </div>
             </DialogTitle>
+            <DialogDescription>
+              <div className="flex flex-col text-center">
+                <span>I would like your feedback to improve the website.</span>
+                <span>How would you rate the website?</span>  
+              </div>            
+            </DialogDescription>
           </DialogHeader>
-          <h2>Hello 😁</h2>
+          <ReactStars
+            count={5}
+            onChange={(newRating)=>{setRating(newRating)}}
+            size={50}
+            isHalf={true}
+            emptyIcon={<box-icon name='star'></box-icon>}
+            fullIcon={<box-icon type='solid' name='star-half'></box-icon>}
+            activeColor="#fde047"
+            classNames={"w-full flex justify-around"}
+            a11y={false}
+          />
+          <label htmlFor="feedback">
+            <Textarea onChange={(e)=>{setFeedback(e.target.value)}} placeholder="Type your feedback here."  name="feedback" id="feedback" style={{resize:'none'}}/>
+          </label>
+          <Button onClick={()=>{console.log('clicked')}}>Send</Button>
         </DialogContent>
       </Dialog>
     </>
