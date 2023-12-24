842e525d4c12d36a40d52b298f75d3abc922a0c5
imadeekosatria <imadeekosatriawiguna@gmail.com>
2023-12-24 19:08:57 +0800

    fix config firebase

--- utils/firebase.js
+++ utils/firebase.js
@@ -15,7 +15,6 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
 };
 
